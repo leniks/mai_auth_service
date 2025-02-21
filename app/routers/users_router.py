ec8eb61c6508f1121425c036d75471c7bff97ddc
leoan <anciborko04@mail.ru>
2025-02-22 01:37:42 +0300

    added kafka

--- app/routers/users_router.py
+++ app/routers/users_router.py
@@ -1,11 +1,12 @@
 from fastapi import APIRouter, HTTPException, status, Response, Depends
 
-from app.schemas import User_schema
 from app.services.users_service import UsersService
 from app.schemas.User_schema import User, RegisterUser, AuthUser
 from app.services.auth_service import get_password_hash, authenticate_user_by_username, create_access_token, \
     get_current_user
 from typing import Optional, List
+from app.services.kafka_producer import get_kafka_producer
+import json
 
 
 router = APIRouter(prefix='/users', tags=['Работа с пользователями'])
@@ -45,15 +46,24 @@ async def add_user(user_add: RegisterUser)  -> dict:
     return {'message': 'Вы успешно зарегистрированы!'}
 
 @router.post("/login/", summary="Вход в аккаунт")
-async def auth_user(response: Response, user_data: AuthUser):
-    check = await authenticate_user_by_username(username=user_data.username, password=user_data.password)
+async def auth_user(response: Response, user_data: AuthUser, producer = Depends(get_kafka_producer)):
+    user = await authenticate_user_by_username(username=user_data.username, password=user_data.password)
 
-    if check is None:
+    if user is None:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
                             detail='Неверная почта или пароль')
 
-    access_token = create_access_token({"sub": str(check.id)})
+    access_token = create_access_token({"sub": str(user.id)})
     response.set_cookie(key="users_access_token", value=access_token, httponly=False)
+
+    event = {
+        "eventType": "UserRegistered",
+        "userId": user.id,
+        "email": user.user_mail
+    }
+    producer.produce('user_events', value=json.dumps(event))
+    producer.flush()
+
     return {'access_token': access_token}
 
 @router.get("/me", summary="Информация о текущем пользователе")
